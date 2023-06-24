2d2b59d263b78ebc7c2c6698afc5f1b98cf85245
enigmaSpy <soczekv18@gmail.com>
2023-06-24 19:58:19 +0200

    better UI

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const productsDisplay = document.querySelector('#products');
+const productsDisplay = document.querySelector('.products');
 
 const url = 'http://localhost:8000/products';
 let render = '';
@@ -8,12 +8,14 @@ fetch('http://localhost:8000/products')
     .then(data => {
         data.forEach(product => {
             render += `
-               <div class='product__item'>
+            <a href='${product.url}' target='_blank' class='product__item'>
                 <span class='products__category'>${product.category}</span>
-                <h3 class='products__title'><a href='${product.url}' target='_blank'>${product.title}</a></h3>
-                <p class='products__price'>${product.price}</p>
+                <div class='products__imgWrapper'>
                 <img src='${product.img}' alt='product-img' class='products__img'>
-               </div>
+                </div>
+                <h3 class='products__title'>${product.title}</h3>
+                <p class='products__price'>${product.price}</p>
+               </a>
             `
         });
         productsDisplay.innerHTML = render;
